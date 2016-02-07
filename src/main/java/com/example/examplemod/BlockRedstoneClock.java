8bed5985a0014207477323a52585d003eb0a79b9
nui <nui@hoge.hoge>
2016-02-07 21:13:04 +0900

    enable clock of BlockRedstoneClock

--- src/main/java/com/example/examplemod/BlockRedstoneClock.java
+++ src/main/java/com/example/examplemod/BlockRedstoneClock.java
@@ -4,6 +4,9 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+
+import java.util.Random;
 
 /**
  * Created by frevmet on 16/02/07.
@@ -31,6 +34,24 @@ public class BlockRedstoneClock extends Block {
 
     @Override
     public int isProvidingWeakPower(IBlockAccess world, int x, int y, int z, int side) {
-        return 15;
+        return world.getBlockMetadata(x, y, z);
+    }
+
+    @Override
+    public void updateTick(World world, int x, int y, int z, Random random) {
+        int meta = world.getBlockMetadata(x, y, z);
+
+        if (meta == 0) {
+            world.setBlockMetadataWithNotify(x, y, z, 15, 3);
+        } else {
+            world.setBlockMetadataWithNotify(x, y, z, 0, 3);
+        }
+
+        world.scheduleBlockUpdateWithPriority(x, y, z, this, 5, 100);
+    }
+    
+    @Override
+    public void onBlockAdded(World world, int x, int y, int z) {
+        world.scheduleBlockUpdateWithPriority(x, y, z, this, 5, 100);
     }
 }
