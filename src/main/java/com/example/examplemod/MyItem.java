91c025e2fb325b64259bb5f4ce015784f402c7e6
nui <nui@hoge.hoge>
2016-02-08 23:00:37 +0900

    remove unecessary comments

--- src/main/java/com/example/examplemod/MyItem.java
+++ src/main/java/com/example/examplemod/MyItem.java
@@ -1,11 +1,7 @@
 package com.example.examplemod;
 
- //       import net.minecraft.creativetab.CreativeTabs;
- //       import net.minecraft.item.Item;
-        import net.minecraft.block.Block;
         import net.minecraft.entity.EntityLivingBase;
         import net.minecraft.entity.player.EntityPlayer;
-        import net.minecraft.init.Blocks;
         import net.minecraft.item.ItemStack;
         import net.minecraft.item.ItemSword;
         import net.minecraft.potion.Potion;
@@ -21,7 +17,6 @@ public class MyItem extends ItemSword {
         super(EnumHelper.addToolMaterial("Original_Sword", 4, 2000, 16.0f, 1.0f, 22));
         setUnlocalizedName(ExampleMod.MODID + "_Original_Sword");
         setTextureName(ExampleMod.MODID + ":Original_Sword");
-        //setCreativeTab(CreativeTabs.tabTools);
     }
 
     @Override
